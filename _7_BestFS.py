6b5107d71ead19ae7ae0264a027be2226959ac88
Muhammad Muneeb ur Rehman <129494978+SufiMuneeb0210@users.noreply.github.com>
2023-05-18 18:40:00 +0500

    Second Commit

--- _7_BestFS.py
+++ _7_BestFS.py
@@ -4,6 +4,7 @@ from collections import deque
 class Graph_bestfs:
     def __init__(self, directed=False):
         self.graph = {}
+        self.huristics = {}
         self.directed = directed
 
     def add_edge(self, node1, node2, weight):
@@ -16,33 +17,14 @@ class Graph_bestfs:
         if not self.directed:
             self.graph[node2].append((node1, weight))
 
+
+    def set_huristics(self, heuristics={}):
+        self.heuristics = heuristics
+        
+    
     @staticmethod
     def print_path(path):
         if path:
             print(' -> '.join(path))
         else:
             print('No path found.')
-
-    def search(self, start, goal):
-        visited = set()
-        priority_queue = deque([(0, start, [])])
-        while priority_queue:
-            _, current_node, path = priority_queue.popleft()
-            visited.add(current_node)
-
-            if current_node == goal:
-                return path + [current_node]
-
-            neighbors = self.graph.get(current_node, [])
-            neighbors.sort(key=lambda x: self.heuristic(x[0], goal))
-            for neighbor, _ in neighbors:
-                if neighbor not in visited:
-                    priority_queue.append(
-                        (self.heuristic(neighbor, goal), neighbor, path + [current_node]))
-
-        return []
-
-    def heuristic(self, node, goal):
-        x1, y1 = node
-        x2, y2 = goal
-        return abs(x1 - x2) + abs(y1 - y2)
